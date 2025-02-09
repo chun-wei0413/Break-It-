9b4edd33bcc2959a294ccc10e5098a1ec2c57060
李俊威 <113598009@cc.ntut.edu.tw>
2025-02-09 20:00:24 +0800

    [Refatoring] Add test package.

---
+++ src/test/java/org/frank/entertainment/test.java
@@ -0,0 +1,13 @@
+package org.frank.entertainment;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class test {
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
